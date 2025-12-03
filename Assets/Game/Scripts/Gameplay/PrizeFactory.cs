a53adbbaf7e11037a766a93271940991963ed865
Dennis-interdigital <dennis@inter-digital.org>
2025-12-03 11:08:32 +0700

    add spawn prize
    change prefab prize scales

--- Assets/Game/Scripts/Gameplay/PrizeFactory.cs
+++ Assets/Game/Scripts/Gameplay/PrizeFactory.cs
@@ -18,6 +18,25 @@ public class PrizeFactory : MonoBehaviour
         {
             activePrizeList[i].Init(this);
         }
+
+        SpawnPrize(10);
+    }
+
+    public void SpawnPrize(int amount)
+    {
+        StartCoroutine(SpawningPrize(amount));
+    }
+
+    IEnumerator SpawningPrize(int amount)
+    {
+        int rndIndex;
+        for (int i = 0; i < amount; i++)
+        {
+            rndIndex = Random.Range(0, prefabPrizes.Length);
+            Prize prefab = prefabPrizes[rndIndex];
+            Instantiate(prefab, transform);
+            yield return new WaitForSeconds(0.3f);
+        }
     }
 
     public void ReturnPrize(Prize prize)
